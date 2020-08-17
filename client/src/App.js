c4d0054e95c0e453c77202779c371d915cc54ea8
Joel Olivero <joelolivero@outlook.com>
2020-08-17 09:52:46 -0400

    creating more one the profile page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layouts/Alert';
 import Dashboard from './components/layouts/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
@@ -36,6 +37,11 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
               dashboard
             </Switch>
           </section>
